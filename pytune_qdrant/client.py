73754cf0a19ec7031404e419fe101e7396f18c79
gdefombelle <gabriel.de.fombelle@gmail.com>
2025-09-15 18:06:43 +0200

    first commit macbook

--- pytune_qdrant/client.py
+++ pytune_qdrant/client.py
@@ -1,5 +1,7 @@
 from qdrant_client import QdrantClient
-from pytune_configuration.sync_config_singleton import config
+from pytune_configuration.sync_config_singleton import SimpleConfig, config
+
+config = config or SimpleConfig()
 
 client = QdrantClient(
     host=config.QDRANT_HOST,
